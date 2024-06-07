2d637d4ba3cf719bfb2b632ff73762fa6ced8041
Emil Kristensen <emil00y1@stud.kea.dk>
2024-06-07 14:14:56 +0200

    updated list

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -11,8 +11,10 @@ const buttonVariants = cva(
       variant: {
         default: "bg-ibgreen-400 text-ibsilver-600 hover:bg-ibgreen-200 group",
         destructive: "bg-ibred-400 text-ibsilver-100 hover:bg-ibred-400/70",
-        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -20,7 +22,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-8 w-8",
       },
     },
     defaultVariants: {
@@ -30,10 +32,18 @@ const buttonVariants = cva(
   }
 );
 
-const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button";
-  return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
-});
+const Button = React.forwardRef(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
--- src/components/views/ListView.jsx
+++ src/components/views/ListView.jsx
@@ -19,9 +19,17 @@ export default async function ListView() {
       <h2 className="bold text-xl">ListView</h2>
       <ul>
         {productCards.map((card) => (
-          <li key={card.id} className="p-2 border-b-2">
-            <p className="text-sm text-ibsilver-400">{card.parent}</p>
-            <h3 className="text-base font-medium">{card.title}</h3>
+          <li key={card.id} className="p-2 border-b-2 flex gap-2">
+            <div>
+              <p className="text-sm text-ibsilver-400">{card.parent}</p>
+              <h3 className="text-base font-medium break-words max-w-[180px]">
+                {card.title}
+              </h3>
+            </div>
+            <div className="ml-auto flex gap-2">
+              <Button size="icon" variant="destructive" />
+              <Button size="icon" />
+            </div>
           </li>
         ))}
         <CaseList />
