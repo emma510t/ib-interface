b28493a502fbb5277eec18d03e486398b8a3b312
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2024-06-07 12:26:34 +0200

    setup

---
+++ src/components/views/CaseForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CaseForm({ className }) {
+  return (
+    <section className={`bg-ibsilver-400 ${className}`}>
+      <h2>CaseForm</h2>
+    </section>
+  );
+}
---
+++ src/components/views/ListView.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ListView() {
+  return (
+    <section className="bg-ibsilver-300">
+      <h2>ListView</h2>
+    </section>
+  );
+}
